e1109ecccd0905dd32277755bbc711a5dcc1f82a
nannal <d.abbott@nannal.com>
2019-07-25 11:00:16 +0300

    Removed uneeded semi-colons and ran though beautify

--- video-processor-cmds.js
+++ video-processor-cmds.js
@@ -1,11 +1,11 @@
-const shell = require('shelljs');
-const ipfsAPI = require('ipfs-http-client');
-const fs = require('fs');
+const shell = require('shelljs')
+const ipfsAPI = require('ipfs-http-client')
+const fs = require('fs')
 const hbjs = require('handbrake-js')
 
-var ipfsIp = process.env.IPFSIP || '127.0.0.1';
-var ipfsPort = process.env.IPFSPORT || '5001';
-var ipfsProtocol = process.env.IPFSPROTOCOL || 'http';
+var ipfsIp = process.env.IPFSIP || '127.0.0.1'
+var ipfsPort = process.env.IPFSPORT || '5001'
+var ipfsProtocol = process.env.IPFSPROTOCOL || 'http'
 
 var cmds = {
 
@@ -15,7 +15,7 @@ var cmds = {
 			str1 = "ffprobe -v error -of default=nw=1 -show_entries stream_tags=rotate:format=size,duration:stream=index,codec_name,pix_fmt,height,width,duration,nb_frames,avg_frame_rate,bit_rate "
 			str2 = " -print_format json"
 
-			return str1+filePath+str2
+			return str1 + filePath + str2
 		}
 
 	},
@@ -24,88 +24,92 @@ var cmds = {
 		// uploads file to ipfs, second parameter is the property to update within encoder response
 		ipfsUpload: (filePath, justHash, prop) => {
 			//Connceting to our http api
-			const ipfs = ipfsAPI(ipfsIp, ipfsPort, {protocol: ipfsProtocol})
-			//let videoFile = fs.readFileSync(filePath);
-			//let testBuffer = new Buffer.from(videoFile);
+			const ipfs = ipfsAPI(ipfsIp, ipfsPort, {
+				protocol: ipfsProtocol
+			})
+			//let videoFile = fs.readFileSync(filePath)
+			//let testBuffer = new Buffer.from(videoFile)
 
-			ipfs.add(videoFile, {"only-hash": justHash}, function (err, file) {
+			ipfs.add(videoFile, {
+				"only-hash": justHash
+			}, function(err, file) {
 
 				if (err) {
-					console.log(err);
-					process.exit();
+					console.log(err)
+					process.exit()
 				}
 				// updating relevant encoder response fields
-				cmds.setObjPropToValue(cmds.encoderResponse, prop+".progress", "100.00%");
-				cmds.setObjPropToValue(cmds.encoderResponse, prop+".lastTimeProgress", Date());
-				cmds.setObjPropToValue(cmds.encoderResponse, prop+".step", "success");
-				cmds.setObjPropToValue(cmds.encoderResponse, prop+".hash", file[0].hash);
-				cmds.setObjPropToValue(cmds.encoderResponse, prop+".fileSize", file[0].size);
+				cmds.setObjPropToValue(cmds.encoderResponse, prop + ".progress", "100.00%")
+				cmds.setObjPropToValue(cmds.encoderResponse, prop + ".lastTimeProgress", Date())
+				cmds.setObjPropToValue(cmds.encoderResponse, prop + ".step", "success")
+				cmds.setObjPropToValue(cmds.encoderResponse, prop + ".hash", file[0].hash)
+				cmds.setObjPropToValue(cmds.encoderResponse, prop + ".fileSize", file[0].size)
 
-			});
+			})
 
 		}
 	},
 	sprite_cmds: {
 
 		sprite: (filePath, vidLength, resDir) => {
-			var splitCmd = cmds.sprite_cmds.createVideoSplitCmd(filePath, vidLength, resDir);
-			var montCmd = cmds.sprite_cmds.createMontageCmd(resDir);
-			cmds.sprite_cmds.createSprite(splitCmd, montCmd);
+			var splitCmd = cmds.sprite_cmds.createVideoSplitCmd(filePath, vidLength, resDir)
+			var montCmd = cmds.sprite_cmds.createMontageCmd(resDir)
+			cmds.sprite_cmds.createSprite(splitCmd, montCmd)
 		},
 
 		// splits video into images
 		createVideoSplitCmd: (filePath, vidLength, resDir) => {
 
 			// if (vidLength > 600) {
-			// 	var frameRate = 100/vidLength;
+			// 	var frameRate = 100/vidLength
 			// } else {
-			// 	var frameRate = 1;
+			// 	var frameRate = 1
 			// }
 
-			let frameRate = 100/vidLength;
+			let frameRate = 100 / vidLength
 
-			return `ffmpeg -y -i `+filePath+ ` -r `+frameRate+` -vf scale=128:72 -f image2 `+resDir+`/img%03d`
+			return `ffmpeg -y -i ` + filePath + ` -r ` + frameRate + ` -vf scale=128:72 -f image2 ` + resDir + `/img%03d`
 		},
 
 		// concatenates all the images together
 		createMontageCmd: (resDir) => {
-			return `montage -mode concatenate -tile 1x `+resDir+`/* `+resDir+`/sprite.png`
+			return `montage -mode concatenate -tile 1x ` + resDir + `/* ` + resDir + `/sprite.png`
 		},
 
 		createSprite: (splitCmd, montCmd) => {
 
-      var timeoutfunc = setTimeout(()=>{
-          console.log("Sprite makin timed out")
-          console.log("Killing container")
-          process.exit();
-        }, 600000);
+			var timeoutfunc = setTimeout(() => {
+				console.log("Sprite makin timed out")
+				console.log("Killing container")
+				process.exit()
+			}, 600000)
 
-			shell.exec(splitCmd,function(code, stdout, stderr) {
+			shell.exec(splitCmd, function(code, stdout, stderr) {
 
 				// code isn't 0 if error occurs
 				if (code) {
-					console.log(stderr);
-					process.exit();
+					console.log(stderr)
+					process.exit()
 				} else {
-					shell.exec(montCmd, function(code, stdout, stderr){
+					shell.exec(montCmd, function(code, stdout, stderr) {
 						// code isn't 0 if error occurs
 						if (code) {
-							console.log(stderr);
-							process.exit();
+							console.log(stderr)
+							process.exit()
 						} else {
-              clearTimeout(timeoutfunc)
-              console.log("sprite completed")
+							clearTimeout(timeoutfunc)
+							console.log("sprite completed")
 							//if no errors, update relevant encoder response fields and upload to ipfs
-							cmds.encoderResponse.sprite.spriteCreation.progress = "100.00%";
-							cmds.encoderResponse.sprite.spriteCreation.lastTimeProgress = Date();
-							cmds.encoderResponse.sprite.spriteCreation.step = "Success";
-							cmds.ipfs_cmds.ipfsUpload("./sprite/sprite.png", true, 'sprite.ipfsAddSprite');
-							return stdout;
+							cmds.encoderResponse.sprite.spriteCreation.progress = "100.00%"
+							cmds.encoderResponse.sprite.spriteCreation.lastTimeProgress = Date()
+							cmds.encoderResponse.sprite.spriteCreation.step = "Success"
+							cmds.ipfs_cmds.ipfsUpload("./sprite/sprite.png", true, 'sprite.ipfsAddSprite')
+							return stdout
 						}
-					});
+					})
 				}
 
-			});
+			})
 		}
 
 	},
@@ -123,11 +127,11 @@ var cmds = {
 		},
 
 		changeSettings: (filePath, resName, maxWidth, maxHeight) => {
-			let settings = cmds.encoder_cmds.encoderSettings;
-			settings.input = filePath;
-			settings.output = resName;
-			settings.maxWidth = maxWidth;
-			settings.maxHeight = maxHeight;
+			let settings = cmds.encoder_cmds.encoderSettings
+			settings.input = filePath
+			settings.output = resName
+			settings.maxWidth = maxWidth
+			settings.maxHeight = maxHeight
 
 			return settings
 
@@ -135,36 +139,36 @@ var cmds = {
 
 		encode: (settings, encodedVideoIndex, cb) => {
 
-      var timeoutfunc = setTimeout(()=>{
-          console.log("Encoder timed out")
-          console.log("Killing container")
-          process.exit();
-        },1800000);
-			var noop = function(){};
-			cb = cb || noop;
+			var timeoutfunc = setTimeout(() => {
+				console.log("Encoder timed out")
+				console.log("Killing container")
+				process.exit()
+			}, 1800000)
+			var noop = function() {}
+			cb = cb || noop
 
-			let propIpfs = 'encodedVideos[' + String(encodedVideoIndex) + '].ipfsAddEncodeVideo';
-			var outputName = settings.output;
+			let propIpfs = 'encodedVideos[' + String(encodedVideoIndex) + '].ipfsAddEncodeVideo'
+			var outputName = settings.output
 
 			hbjs.spawn(settings)
 				.on('error', err => {
-					// console.log(err);
-					cmds.encoderResponse.encodedVideos[encodedVideoIndex].encode.errorMessage = err;
-          console.log("Exiting process, encoding error");
-					process.exit();
+					// console.log(err)
+					cmds.encoderResponse.encodedVideos[encodedVideoIndex].encode.errorMessage = err
+					console.log("Exiting process, encoding error")
+					process.exit()
 				})
 				.on('progress', progress => {
-          console.log("Encoding progress, video index: "+ encodedVideoIndex)
-					cmds.encoderResponse.encodedVideos[encodedVideoIndex].encode.progress = String(progress.percentComplete)+"%";
-					cmds.encoderResponse.encodedVideos[encodedVideoIndex].encode.lastTimeProgress = Date();
+					console.log("Encoding progress, video index: " + encodedVideoIndex)
+					cmds.encoderResponse.encodedVideos[encodedVideoIndex].encode.progress = String(progress.percentComplete) + "%"
+					cmds.encoderResponse.encodedVideos[encodedVideoIndex].encode.lastTimeProgress = Date()
 				})
 				.on('complete', () => {
-          console.log("Encoding completed, video index: " + encodedVideoIndex)
-          clearTimeout(timeoutfunc)
+					console.log("Encoding completed, video index: " + encodedVideoIndex)
+					clearTimeout(timeoutfunc)
 					// when complete, upload to ipfs
-					cmds.ipfs_cmds.ipfsUpload(outputName, true, propIpfs);
-					cb();
-				});
+					cmds.ipfs_cmds.ipfsUpload(outputName, true, propIpfs)
+					cb()
+				})
 		}
 	},
 	encoderResponse: {
@@ -208,7 +212,7 @@ var cmds = {
 	// adds encoded video data fields to encoder response
 	addEncodedVideoData: (encodeSize) => {
 
-		var num = encodeSize.length;
+		var num = encodeSize.length
 
 		for (let i = 0; i < num; i++) {
 			cmds.encoderResponse.encodedVideos.push({
@@ -220,95 +224,96 @@ var cmds = {
 					step: "Waiting",
 					positionInQueue: null
 				},
-					ipfsAddEncodeVideo: {
-						progress: null,
-						encodeSize: "",
-						lastTimeProgress: null,
-						errorMessage: null,
-						step: "init",
-						positionInQueue: null,
-						hash: null,
-						fileSize: null
-					}
-			});
-			cmds.encoderResponse.encodedVideos[i].encode.encodeSize = encodeSize[i];
-			cmds.encoderResponse.encodedVideos[i].ipfsAddEncodeVideo.encodeSize = encodeSize[i];
+				ipfsAddEncodeVideo: {
+					progress: null,
+					encodeSize: "",
+					lastTimeProgress: null,
+					errorMessage: null,
+					step: "init",
+					positionInQueue: null,
+					hash: null,
+					fileSize: null
+				}
+			})
+			cmds.encoderResponse.encodedVideos[i].encode.encodeSize = encodeSize[i]
+			cmds.encoderResponse.encodedVideos[i].ipfsAddEncodeVideo.encodeSize = encodeSize[i]
 		}
 
 	},
 
 	// function for setting deep nested object property values
 	setObjPropToValue: (obj, path, value) => {
-		var i;
-		path = path.split(/(?:\.|\[|\])+/);
+		var i
+		path = path.split(/(?:\.|\[|\])+/)
 		for (i = 0; i < path.length - 1; i++)
-			obj = obj[path[i]];
+			obj = obj[path[i]]
 
-		obj[path[i]] = value;
+		obj[path[i]] = value
 	},
 
 	moveFiles: (filePath, numOfEncodedVids) => {
 
-		var oldEncodedVidPaths = ["fileres240.mp4", "fileres480.mp4"];
+		var oldEncodedVidPaths = ["fileres240.mp4", "fileres480.mp4"]
 
-		var is = fs.createReadStream(filePath);
-		var os = fs.createWriteStream("./longtermstore/" + cmds.encoderResponse.ipfsAddSourceVideo.hash);
+		var is = fs.createReadStream(filePath)
+		var os = fs.createWriteStream("./longtermstore/" + cmds.encoderResponse.ipfsAddSourceVideo.hash)
 
-		is.pipe(os);
-		is.on('end',function() {
-		    fs.unlinkSync(filePath);
-		});
+		is.pipe(os)
+		is.on('end', function() {
+			fs.unlinkSync(filePath)
+		})
 
-		for(let i=0; i<numOfEncodedVids; i++){
-			is = fs.createReadStream(oldEncodedVidPaths[i]);
-			os = fs.createWriteStream("./longtermstore/" + cmds.encoderResponse.encodedVideos[i].ipfsAddEncodeVideo.hash);
+		for (let i = 0; i < numOfEncodedVids; i++) {
+			is = fs.createReadStream(oldEncodedVidPaths[i])
+			os = fs.createWriteStream("./longtermstore/" + cmds.encoderResponse.encodedVideos[i].ipfsAddEncodeVideo.hash)
 
-			is.pipe(os);
-			is.on('end',function() {
-			    fs.unlinkSync(oldEncodedVidPaths[i]);
-			});
+			is.pipe(os)
+			is.on('end', function() {
+				fs.unlinkSync(oldEncodedVidPaths[i])
+			})
 		}
 
-		is = fs.createReadStream("./sprite/sprite.png");
-		os = fs.createWriteStream("./longtermstore/" + cmds.encoderResponse.sprite.ipfsAddSprite.hash);
+		is = fs.createReadStream("./sprite/sprite.png")
+		os = fs.createWriteStream("./longtermstore/" + cmds.encoderResponse.sprite.ipfsAddSprite.hash)
 
-		is.pipe(os);
-		is.on('end',function() {
-		    fs.unlinkSync("./sprite/sprite.png");
-		});
+		is.pipe(os)
+		is.on('end', function() {
+			fs.unlinkSync("./sprite/sprite.png")
+		})
 
 	},
 
 	// checking encoder response values to ensure everything is done before setting finished to true
 	checkIfFinished: (filePath) => {
 		var numOfEncodedVids = cmds.encoderResponse.encodedVideos.length
-		console.log("We should have encoded: "+numOfEncodedVids+" videos");
-		var func = setInterval(()=>{
+		console.log("We should have encoded: " + numOfEncodedVids + " videos")
+		var func = setInterval(() => {
 
 			// creating an array of encoded video hashes to iterate through in the "if"
-			var encodedVidsHash = [];
-			for (let i = 0; i < numOfEncodedVids; i++)
-			{
-				encodedVidsHash.push(cmds.encoderResponse.encodedVideos[i].ipfsAddEncodeVideo.hash);
+			var encodedVidsHash = []
+			for (let i = 0; i < numOfEncodedVids; i++) {
+				encodedVidsHash.push(cmds.encoderResponse.encodedVideos[i].ipfsAddEncodeVideo.hash)
 			}
 
-			if (cmds.encoderResponse.ipfsAddSourceVideo.hash && cmds.encoderResponse.sprite.ipfsAddSprite.hash && encodedVidsHash.every((hash) => {return hash})){
-				clearInterval(func);
-        console.log("Moving files to long term storage")
-        console.log(cmds.encoderResponse)
+			if (cmds.encoderResponse.ipfsAddSourceVideo.hash && cmds.encoderResponse.sprite.ipfsAddSprite.hash && encodedVidsHash.every((hash) => {
+					return hash
+				})) {
+				clearInterval(func)
+				console.log("Moving files to long term storage")
+				console.log(cmds.encoderResponse)
 				cmds.moveFiles(filePath, numOfEncodedVids)
 				// wait before setting finished to true and ending process
-				setTimeout(()=>{
-					cmds.encoderResponse.finished = true;
-          console.log("Encoder finished")
-				}, 1000);
-				setTimeout(()=>{
-          console.log("Killing container")
-					process.exit();
-				},10000);
+				setTimeout(() => {
+					cmds.encoderResponse.finished = true
+					console.log("Encoder finished")
+				}, 1000)
+				setTimeout(() => {
+					console.log("Killing container")
+					process.exit()
+				}, 10000)
 			}
 
-		},2000);
+		}, 2000)
 
 	},
 
--- video-processor.js
+++ video-processor.js
@@ -1,80 +1,82 @@
-const http = require('http');
-const shell = require('shelljs');
-const uuidv4 = require('uuid/v4');
-const formidable = require('formidable');
-var cmds = require('./video-processor-cmds.js');
+const http = require('http')
+const shell = require('shelljs')
+const uuidv4 = require('uuid/v4')
+const formidable = require('formidable')
+var cmds = require('./video-processor-cmds.js')
 
 // cross-origin resource sharing variable
-corsVar = process.env.CORSVAR || 'https://d.tube';
+corsVar = process.env.CORSVAR || 'https://d.tube'
 
 // variable to assure only one upload request happens
-var reqhappened = false;
+var reqhappened = false
 
 // generated token
-const genToken = uuidv4();
+const genToken = uuidv4()
 
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
 
-	res.setHeader('access-control-allow-headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
-	res.setHeader('access-control-allow-origin', corsVar);
-	res.setHeader('access-control-allow-credentials', 'true');
-	res.setHeader('Content-Type','application/json; charset=utf-8');
-	res.setHeader('access-control-max-age','600');
+	res.setHeader('access-control-allow-headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
+	res.setHeader('access-control-allow-origin', corsVar)
+	res.setHeader('access-control-allow-credentials', 'true')
+	res.setHeader('Content-Type', 'application/json; charset=utf-8')
+	res.setHeader('access-control-max-age', '600')
 
 	if (req.url == '/getStatus') {
-    console.log("getStatus Hit")
-		res.statusCode = 200;
-		res.end('{"version":"0.7.5","currentWaitingInQueue":{"audioCpuToEncode":0,"videoGpuToEncode":0,"audioVideoCpuToEncode":0,"spriteToCreate":0,"ipfsToAdd":0}}');
+		console.log("getStatus Hit")
+		res.statusCode = 200
+		res.end('{"version":"0.7.5","currentWaitingInQueue":{"audioCpuToEncode":0,"videoGpuToEncode":0,"audioVideoCpuToEncode":0,"spriteToCreate":0,"ipfsToAdd":0}}')
 	}
 
-	 // sending encoder progress to user
+	// sending encoder progress to user
 	if (req.url.match(/\/getProgressByToken.*/)) {
 
-		res.end(JSON.stringify(cmds.encoderResponse));
+		res.end(JSON.stringify(cmds.encoderResponse))
 	}
 
 	// file upload
 	if (req.url == '/uploadVideo?videoEncodingFormats=240p,480p,720p,1080p&sprite=true' && !reqhappened) {
 
-		if (req.method === 'OPTIONS'){
-      console.log("uploadVideo Options Hit")
-			res.statusCode = 204;
-			res.end();
+		if (req.method === 'OPTIONS') {
+			console.log("uploadVideo Options Hit")
+			res.statusCode = 204
+			res.end()
 
 		} else {
-      var fileUploadLimit = setTimeout(()=>{
-          console.log("Upload timed out")
-          console.log("Killing container")
-          process.exit();
-        }, 3600000);
+			var fileUploadLimit = setTimeout(() => {
+				console.log("Upload timed out")
+				console.log("Killing container")
+				process.exit()
+			}, 3600000)
 
-			res.statusCode = 200;
-      console.log("uploadVideo Non-Options Hit")
+			res.statusCode = 200
+			console.log("uploadVideo Non-Options Hit")
 
-			var form = new formidable.IncomingForm();
+			var form = new formidable.IncomingForm()
 
 			//Sane Form options
-			form.maxFields = 1;
-			form.encoding = 'utf-8';
-			form.maxFileSize = '4096000000';
+			form.maxFields = 1
+			form.encoding = 'utf-8'
+			form.maxFileSize = '4096000000'
 
-			form.parse(req, function (err, fields, files) {
-			});
+			form.parse(req, function(err, fields, files) {})
 
 			// file is moved to upload folder and renamed to uuid
-			form.on('fileBegin', function (name, file){
+			form.on('fileBegin', function(name, file) {
 
-        reqhappened = true;
-        console.log("File upload Began")
-				file.path = "./upload/" + genToken;
-			});
+				reqhappened = true
+				console.log("File upload Began")
+				file.path = "./upload/" + genToken
+			})
 
 
-			form.on('file', function (name, file){
-        console.log("File upload Finished, setting reqhappened")
-        clearTimeout(fileUploadLimit)
+			form.on('file', function(name, file) {
+				console.log("File upload Finished, setting reqhappened")
+				clearTimeout(fileUploadLimit)
 				//frontend needs to know if upload was successful and receive the token
-				var successResponse = { success: "", token: ""};
+				var successResponse = {
+					success: "",
+					token: ""
+				}
 
 				// check if file is valid
 				shell.exec(cmds.ffprobe_cmds.createCmdString(file.path), function(code, stdout, stderr) {
@@ -82,76 +84,76 @@ http.createServer(function (req, res) {
 					// code isn't 0 if error occurs
 					if (code) {
 						// if error, success is false, no token, end process
-						console.log(stderr);
-            console.log("Bad File, killing.")
-						successResponse.success = "false";
-						res.end(JSON.stringify(successResponse));
-						process.exit();
+						console.log(stderr)
+						console.log("Bad File, killing.")
+						successResponse.success = "false"
+						res.end(JSON.stringify(successResponse))
+						process.exit()
 					}
 
-					var fileData = JSON.parse(stdout);
+					var fileData = JSON.parse(stdout)
 
 					// if file is valid, success is true and provide token
-					successResponse.success = "true";
-					successResponse.token = genToken;
-          console.log(successResponse+" sent to user")
-					res.end(JSON.stringify(successResponse));
+					successResponse.success = "true"
+					successResponse.token = genToken
+					console.log(successResponse + " sent to user")
+					res.end(JSON.stringify(successResponse))
 
-					var videoHeight = fileData.streams[0].height;
-					var fileDuration = fileData.format.duration;
+					var videoHeight = fileData.streams[0].height
+					var fileDuration = fileData.format.duration
 
 					// upload source file to ipfs
-          console.log("Uploading to IPFS")
-					cmds.ipfs_cmds.ipfsUpload(file.path, true, "ipfsAddSourceVideo");
+					console.log("Uploading to IPFS")
+					cmds.ipfs_cmds.ipfsUpload(file.path, true, "ipfsAddSourceVideo")
 
 					//create sprite and upload it to ipfs (ipfs upload function is called within sprite function)
-          console.log("Making the sprite")
-					cmds.sprite_cmds.sprite(file.path, fileDuration, "./sprite");
+					console.log("Making the sprite")
+					cmds.sprite_cmds.sprite(file.path, fileDuration, "./sprite")
 
 					// videos under 240 res or longer than 10 minutes not encoded
-					if (videoHeight <= 240 || fileDuration > 600){
-            console.log ("No encoding, checkin if finished")
+					if (videoHeight <= 240 || fileDuration > 600) {
+						console.log("No encoding, checkin if finished")
 						// checks if all procedures are done so that finish status can be set
-						cmds.checkIfFinished(file.path);
+						cmds.checkIfFinished(file.path)
 
-					// videos between 240 and 480 res get encoded to 240 res
-					} else if( videoHeight > 240 && videoHeight <= 480){
-            console.log("Encoding 240p")
+						// videos between 240 and 480 res get encoded to 240 res
+					} else if (videoHeight > 240 && videoHeight <= 480) {
+						console.log("Encoding 240p")
 						// adds data about encoded video to encoder response
-						cmds.addEncodedVideoData(["240p"]);
+						cmds.addEncodedVideoData(["240p"])
 
-						cmds.encoder_cmds.encode(cmds.encoder_cmds.changeSettings(file.path, "fileres240.mp4" , 426, 240), 0);
-						cmds.checkIfFinished(file.path);
+						cmds.encoder_cmds.encode(cmds.encoder_cmds.changeSettings(file.path, "fileres240.mp4", 426, 240), 0)
+						cmds.checkIfFinished(file.path)
 
-					// videos over 480 res get encoded to 240 and 480 res
-					} else if( videoHeight > 480) {
+						// videos over 480 res get encoded to 240 and 480 res
+					} else if (videoHeight > 480) {
 
-            console.log("encoding 240p and 480p")
-						cmds.addEncodedVideoData(["240p","480p"]);
+						console.log("encoding 240p and 480p")
+						cmds.addEncodedVideoData(["240p", "480p"])
 
-						cmds.encoder_cmds.encode(cmds.encoder_cmds.changeSettings(file.path, "fileres240.mp4" , 426, 240), 0, function(){
-							cmds.encoder_cmds.encode(cmds.encoder_cmds.changeSettings(file.path, "fileres480.mp4" , 854, 480), 1);
-						});
+						cmds.encoder_cmds.encode(cmds.encoder_cmds.changeSettings(file.path, "fileres240.mp4", 426, 240), 0, function() {
+							cmds.encoder_cmds.encode(cmds.encoder_cmds.changeSettings(file.path, "fileres480.mp4", 854, 480), 1)
+						})
 
-						cmds.checkIfFinished(file.path);
+						cmds.checkIfFinished(file.path)
 					}
 
-				 });
+				})
 
-			});
+			})
 
 			form.on('error', function(err) {
-				console.error('Error', err);
-				process.exit();
-			});
+				console.error('Error', err)
+				process.exit()
+			})
 
 		}
 
 	} else {
-    res.statusCode = 503;
-		res.end("There's nothing here for you");
+		res.statusCode = 503
+		res.end("There's nothing here for you")
 	}
 
-}).listen(5000, ()=> {
-	console.log("listening on port 5000");
-});
+}).listen(5000, () => {
+	console.log("listening on port 5000")
+})
